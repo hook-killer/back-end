908bbd1f07b4c91995971db6c769148a73ecefe2
donsonioc2010 <whddnjs822@gmail.com>
2023-10-09 16:10:46 +0900

    feat : Cors설정 추가
    - SecurityConfig 작업 진행중이라 추후 SecurityConfig에 Bean등록 작업 추가

--- src/main/java/HookKiller/server/config/ConfigurationProperties.java
+++ src/main/java/HookKiller/server/config/ConfigurationProperties.java
@@ -1,13 +1,17 @@
 package HookKiller.server.config;
 
 
+import HookKiller.server.properties.CorsProperties;
 import HookKiller.server.properties.JwtProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@EnableConfigurationProperties({
-        JwtProperties.class
-})
+@EnableConfigurationProperties(
+    {
+        JwtProperties.class,
+        CorsProperties.class
+    }
+)
 @Configuration
 public class ConfigurationProperties {
 }

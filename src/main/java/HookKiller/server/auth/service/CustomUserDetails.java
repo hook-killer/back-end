e2b3821107ef166c034c46e70d6c76d484d602a1
bongsh0112 <bongsh0112@gmail.com>
2023-10-10 12:44:13 +0900

    edit : CustomUserDetails record로 변경. 확인 바람 #4

--- src/main/java/HookKiller/server/auth/service/CustomUserDetails.java
+++ src/main/java/HookKiller/server/auth/service/CustomUserDetails.java
@@ -1,20 +1,15 @@
 package HookKiller.server.auth.service;
 
 import lombok.Builder;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
-@RequiredArgsConstructor
 @Builder
-public class CustomUserDetails implements UserDetails {
+public record CustomUserDetails(String userId, String role, String nickname) implements UserDetails {
     
-    private final String userId;
-    private final String role;
-
     // 해당 유저의 권한을 리턴하는 메서드!
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -22,43 +17,44 @@ public class CustomUserDetails implements UserDetails {
         collection.add(() -> this.role);
         return collection;
     }
-
+    
     @Override
     public String getPassword() {
         // TODO : getPassword는 나중에 쓴다면 다시 작성
         return this.userId;
     }
-
+    
     @Override
     public String getUsername() {
         return String.valueOf(this.userId);
     }
-
+    
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
-
+    
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
-
+    
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
-
+    
     @Override
     public boolean isEnabled() {
         return true;
     }
     
-    public static CustomUserDetails of(String userId, String role) {
+    public static CustomUserDetails of(String userId, String role, String nickname) {
         return CustomUserDetails.builder()
                 .userId(userId)
                 .role(role)
+                .nickname(nickname)
                 .build();
     }
-
+    
 }

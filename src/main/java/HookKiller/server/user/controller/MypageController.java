6ca080d4e9bde4644d913cd5f4b293ea65c05dd0
donsonioc2010 <whddnjs822@gmail.com>
2023-10-17 13:38:30 +0900

    feat : MyPage 기능 구현 완료
    - 마이페이지 사용자 정보조회
    - 마이페이지 정보수정
    - 마이페이지 썸네일 패스 수정
    - 내가 작성한 댓글 리스트 조회(페이징)
    - 내가 작성한 좋아요 게시물 리스트 조회 (페이징)
    - 내가 작성한 게시물 리스트 조회 (페이징)

--- src/main/java/HookKiller/server/user/controller/MypageController.java
+++ src/main/java/HookKiller/server/user/controller/MypageController.java
@@ -1,17 +1,21 @@
 package HookKiller.server.user.controller;
 
-import HookKiller.server.board.dto.ArticleRequestDto;
-import HookKiller.server.board.entity.Article;
-import HookKiller.server.board.entity.ArticleContent;
-import HookKiller.server.board.service.ArticleContentService;
-import HookKiller.server.user.dto.MyPageRequestDto;
+import HookKiller.server.common.dto.CommonBooleanResultResponse;
+import HookKiller.server.common.type.LanguageType;
+import HookKiller.server.user.dto.MyPageUserResponse;
+import HookKiller.server.user.dto.MyPageUserUpdateRequest;
 import HookKiller.server.user.service.MyPageService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
@@ -20,34 +24,58 @@ import java.util.List;
 public class MypageController {
 
     private final MyPageService myPageService;
-    private final ArticleContentService articleContentService;
 
     /**
      * 마이페이지 조회
-     * @param userId
+     * 애초애 Security에서 사용자 정보없으면 Exception발생하기 떄문에 Paramete가 필요가 없다.
      * @return
      */
-    @GetMapping("/{userId}")
-    public MyPageRequestDto getMyPage(Long userId) {
-        return myPageService.getMyPage(userId);
+    @GetMapping
+    public ResponseEntity<MyPageUserResponse> getMyPage() {
+        return ResponseEntity.ok(myPageService.getMyPage());
+
     }
 
     /**
-     * 정보 수정
+     * 정보 수정, Null로 준 경우 해당 파라미터는 변경하지 않는다.
      * @param requestDto
      */
     @PutMapping
-    public void updatePasswordAndNickname(@RequestBody MyPageRequestDto requestDto) {
-        myPageService.updatePasswordAndNickname(requestDto);
+    public ResponseEntity<CommonBooleanResultResponse> updateUserInfo(@RequestBody MyPageUserUpdateRequest requestDto) {
+        myPageService.updateUserInfo(requestDto);
+        return ResponseEntity.ok(
+                CommonBooleanResultResponse.builder().result(true).message("수정이 완료되었습니다.").build()
+        );
     }
 
     /**
-     * 마이페이지 내가쓴 글
-     *
+     * Thumnail Path변경하고자 하는 경우
+     * @return
      */
-    @GetMapping("/{myList}")
-    public List<ArticleRequestDto> getMyList(Long userId) {
-        return myPageService.getMyList(userId);
+    @PutMapping("/thumnail")
+    public ResponseEntity<CommonBooleanResultResponse> updateUserThumnailPath(@RequestBody MyPageUserUpdateRequest request) {
+        myPageService.updateUserThumbnailPath(request);
+        return ResponseEntity.ok(
+                CommonBooleanResultResponse.builder().result(true).message("수정이 완료되었습니다.").build()
+        );
+    }
+
+    /**
+     * 마이페이지 내가쓴 댓글, 게시물, 좋아요한 게시물의 리스트를 획득 할 수 있는 API
+     * @param searchType 대소문자 구분없이 "reply", "article", "like"
+     * @param page
+     * @param limit
+     * @param request
+     * @return
+     */
+    @GetMapping("/{searchType}")
+    public ResponseEntity<Object> getList(
+            @PathVariable String searchType,
+            @RequestParam(defaultValue = "0", required = false) int page,
+            @RequestParam(defaultValue = "10", required = false) int limit,
+            HttpServletRequest request
+    ) {
+        return ResponseEntity.ok(myPageService.getMyCreatedList(page, limit, searchType, LanguageType.findTypeByRequest(request)));
     }
 
 }
--- src/main/java/HookKiller/server/user/service/MyPageService.java
+++ src/main/java/HookKiller/server/user/service/MyPageService.java
@@ -1,17 +1,32 @@
 package HookKiller.server.user.service;
 
-import HookKiller.server.auth.exception.UserNotFoundException;
 import HookKiller.server.board.dto.ArticleRequestDto;
+import HookKiller.server.board.dto.ReplyResponseDto;
+import HookKiller.server.board.exception.ArticleContentNotFoundException;
+import HookKiller.server.board.exception.ReplyContentNotFoundException;
+import HookKiller.server.board.repository.ArticleContentRepository;
+import HookKiller.server.board.repository.ArticleLikeRepository;
+import HookKiller.server.board.repository.ArticleRepository;
+import HookKiller.server.board.repository.ReplyContentRepository;
+import HookKiller.server.board.repository.ReplyRepository;
+import HookKiller.server.common.exception.BadTypeRequestException;
+import HookKiller.server.common.type.LanguageType;
 import HookKiller.server.common.util.UserUtils;
-import HookKiller.server.user.dto.MyPageRequestDto;
+import HookKiller.server.user.dto.MyPageUserUpdateRequest;
+import HookKiller.server.user.dto.MyPageUserResponse;
 import HookKiller.server.user.entity.User;
 import HookKiller.server.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
+import static HookKiller.server.common.util.SecurityUtils.passwordEncoder;
 
 @Slf4j
 @Service
@@ -19,43 +34,76 @@ import java.util.List;
 public class MyPageService {
 
     private final UserUtils userUtils;
-    private final UserRepository userRepository;
+    private final ReplyRepository replyRepository;
+    private final ArticleRepository articleRepository;
+    private final ArticleLikeRepository articleLikeRepository;
+    private final ReplyContentRepository replyContentRepository;
+    private final ArticleContentRepository articleContentRepository;
 
     @Transactional(readOnly = true)
-    public MyPageRequestDto getMyPage(Long userId) {
-        // 로그인한 사용자 정보를 가져옴
-        User user = userUtils.getUser();
-
-        User requestUser = userRepository.findById(userId).orElseThrow(() -> UserNotFoundException.EXCEPTION);
-
-        if (user.getId().equals(requestUser.getId())) {
-            return MyPageRequestDto.from(requestUser);
-        }
-
-        return null;
+    public MyPageUserResponse getMyPage() {
+        return MyPageUserResponse.from(userUtils.getUser());
     }
 
-    @Transactional
-    public void updatePasswordAndNickname(MyPageRequestDto myPageRequestDto) {
+    @Transactional(propagation = Propagation.NESTED)
+    public void updateUserInfo(MyPageUserUpdateRequest request) {
         User user = userUtils.getUser();
 
-        User requestUser = userRepository.findById(myPageRequestDto.getUserId())
-                .orElseThrow(()-> UserNotFoundException.EXCEPTION);
-
-        if (myPageRequestDto.getPassword() != null) {
-            requestUser.setPassword(myPageRequestDto.getPassword());
-        }
-        if (myPageRequestDto.getNickName() != null) {
-            requestUser.setNickName(myPageRequestDto.getNickName());
-        }
-
-        userRepository.save(requestUser);
-
+        // 변경이 존재하는 경우에만 변경한다.
+        if(request.getEmail() != null && !user.getEmail().equalsIgnoreCase(request.getEmail()))
+            user.setEmail(request.getEmail());
+        if (request.getPassword() != null && !passwordEncoder.matches(request.getPassword(), user.getPassword()))
+            user.setPassword(request.getPassword());
+        if (request.getThumbnail() != null && !user.getThumbnail().equalsIgnoreCase(request.getThumbnail()))
+            user.setThumbnail(request.getThumbnail());
+        if(request.getNickName() != null && !user.getNickName().equals(request.getNickName()))
+            user.setNickName(request.getNickName());
     }
+
+    @Transactional(propagation = Propagation.NESTED)
+    public void updateUserThumbnailPath(MyPageUserUpdateRequest request) {
+        User user = userUtils.getUser();
+        if (request.getThumbnail() != null && !user.getThumbnail().equalsIgnoreCase(request.getThumbnail()))
+            user.setThumbnail(request.getThumbnail());
+    }
+
+
 
     @Transactional(readOnly = true)
-    public List<ArticleRequestDto> getMyList(Long userId) {
-        return null;
+    public Object getMyCreatedList(int page, int limit, String searchType, LanguageType language) {
+        User user = userUtils.getUser();
+        Pageable pageable = PageRequest.of(page, limit);
+        if(searchType.equalsIgnoreCase("ARTICLE")) {
+            return articleRepository.findAllByCreatedUserOrderByCreateAtDesc(user, pageable)
+                    .stream().map(
+                            article -> ArticleRequestDto.of(article, articleContentRepository
+                                    .findByArticleAndLanguage(article, language)
+                                    .orElseThrow(() -> ArticleContentNotFoundException.EXCEPTION))
+                    ).toList();
+        }
+
+        if(searchType.equalsIgnoreCase("REPLY")){
+            return replyRepository.findAllByCreatedUserOrderByCreateAtDesc(user, pageable)
+                    .stream()
+                    .map(reply ->
+                            ReplyResponseDto.of(
+                                    reply,
+                                    replyContentRepository.findByReplyAndLanguage(reply, language)
+                                            .orElseThrow(() -> ReplyContentNotFoundException.EXCEPTION)
+                            )
+                    ).toList();
+        }
+
+        if(searchType.equalsIgnoreCase("LIKE")) {
+            return articleLikeRepository
+                    .findAllByUserOrderByCreateAtDesc(user, pageable)
+                    .map(articleLike -> ArticleRequestDto.of(articleLike.getArticle(), articleContentRepository
+                            .findByArticleAndLanguage(articleLike.getArticle(), language)
+                            .orElseThrow(() -> ArticleContentNotFoundException.EXCEPTION))
+                    ).toList();
+        }
+
+        throw BadTypeRequestException.EXCEPTION;
     }
 
 

d1743d849d9bbb6bbac7d17824cc440a65d409bf
bongsh0112 <bongsh0112@gmail.com>
2023-10-25 03:57:22 +0900

    fix : 댓글 관리자 혹은 작성자만 지울 수 있도록 변경

--- src/main/java/HookKiller/server/board/exception/BoardException.java
+++ src/main/java/HookKiller/server/board/exception/BoardException.java
@@ -5,6 +5,7 @@ import HookKiller.server.common.dto.ErrorDetail;
 import HookKiller.server.common.exception.BaseErrorCode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 import static org.springframework.http.HttpStatus.*;
 
@@ -16,7 +17,9 @@ public enum BoardException implements BaseErrorCode {
   ARTICLE_CONTENT_NOT_FOUND_ERROR(NOT_FOUND.value(), "ArticleContent_404_1", "해당 게시글을 찾을 수 없습니다."),
   ARTICLE_UPDATE_UNAUTHORIZED_USER_ERROR(UNAUTHORIZED.value(), "Article_401_1", "게시물 수정에 대한 권한이 없습니다."),
   ARTICLE_DELETE_UNAUTHORIZED_USER_ERROR(UNAUTHORIZED.value(), "Article_401_2", "게시물 삭제에 대한 권한이 없습니다."),
-  REPLY_CONTENT_NOT_FOUND_ERROR(NOT_FOUND.value(), "ReplyContent_404_1", "해당 댓글을 찾을 수 없습니다.");
+  REPLY_CONTENT_NOT_FOUND_ERROR(NOT_FOUND.value(), "ReplyContent_404_1", "해당 댓글을 찾을 수 없습니다."),
+  REPLY_DELETE_UNAUTHORIZED_USER_ERROR(UNAUTHORIZED.value(), "Reply_404_1", "댓글 삭제에 대한 권한이 없습니다."),
+  ;
 
   private final Integer statusCode;
   private final String errorCode;

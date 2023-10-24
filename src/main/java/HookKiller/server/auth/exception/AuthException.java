bb3b28fbf79865868b09b91443e945c6ba88ed97
wooni <jaeun1212@gmail.com>
2023-10-24 19:25:45 +0900

    fix : Email-verification 미인증시 로그인 수정

--- src/main/java/HookKiller/server/auth/exception/AuthException.java
+++ src/main/java/HookKiller/server/auth/exception/AuthException.java
@@ -4,9 +4,9 @@ import HookKiller.server.common.dto.ErrorDetail;
 import HookKiller.server.common.exception.BaseErrorCode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
 
-import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @Getter
 @AllArgsConstructor
@@ -15,7 +15,8 @@ public enum AuthException implements BaseErrorCode {
   INVALID_TOKEN_ERROR(UNAUTHORIZED.value(), "Token_403_2", "토큰이 만료되었거나 형식에 맞지않은 토큰입니다."),
   TOKEN_NOT_FOUND_ERROR(NOT_FOUND.value(), "Token_404_1", "토큰을 찾을 수 없습니다."),
   USER_NOT_FOUND_ERROR(NOT_FOUND.value(), "User_404_1", "유저를 찾을 수 없습니다."),
-  PASSWORD_INCORRECT_ERROR(NOT_FOUND.value(), "User_404_2", "비밀번호가 다릅니다 다시 입력해 주세요.")
+  PASSWORD_INCORRECT_ERROR(NOT_FOUND.value(), "User_404_2", "비밀번호가 다릅니다 다시 입력해 주세요."),
+  STATUS_NOT_VERIFICATION_ERROR(UNAUTHORIZED.value(), "User_404_3", "이메일 인증을 완료해 주세요.")
   ;
   
   private final Integer statusCode;
